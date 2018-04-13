b000be736af9ac235a1009774e9ba936475f8164
baolijn <libao_fighting@163.com>
2018-04-13 11:27:33 +0800

    kafka java api demo

---
+++ src/main/java/com/libao/kafka/KafkaClientApp.java
@@ -0,0 +1,8 @@
+package com.libao.kafka;
+
+public class KafkaClientApp {
+    public static void main(String[] args) {
+        new KafkaProducer(KafkaProperties.TOPIC).start();
+        new KafkaConsumer(KafkaProperties.TOPIC).start();
+    }
+}
---
+++ src/main/java/com/libao/kafka/KafkaProperties.java
@@ -0,0 +1,9 @@
+package com.libao.kafka;
+
+public class KafkaProperties {
+    public static final String ZK = "10.1.1.11:2181";
+
+    public static final String TOPIC = "hello_topiclibao";
+
+    public static final String BROKER_LIST = "10.1.1.7:9092";
+}
